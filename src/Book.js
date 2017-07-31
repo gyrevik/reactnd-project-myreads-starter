e9e399be5241accd833e2c5cddedca6cdfd2411a
Alex Smotritsky <alex.smotritsky@gmail.com>
2017-07-30 20:41:23 -0400

    feat: integrating prop data with html/css search grid

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,12 @@ function Book(props) {
         <li>
             <div className="book">
                 <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                <div className="book-cover" style={{ 
+                    width: 128, 
+                    height: 193, 
+                    backgroundImage: `url(${props.value.imageLinks.thumbnail})`
+                    }}>
+                </div>
                 <div className="book-shelf-changer">
                     <select>
                         <option value="none" disabled>Move to...</option>
@@ -17,7 +22,8 @@ function Book(props) {
                 </div>
                 </div>
                 <div className="book-title">{props.value.title}</div>
-                <div className="book-authors">{props.value.authors}</div>
+                <div className="book-authors">{props.value.authors && props.value.authors.join(', ')}</div>
+                <div>{}</div>
             </div>
         </li>                
     );
